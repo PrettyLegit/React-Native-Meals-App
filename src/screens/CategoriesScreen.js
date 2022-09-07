a86dcd0229af6f3106d36609f6acd322c734886d
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-09-07 17:20:42 -0400

    Initial commit

---
+++ src/screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import { CATEGORIES } from "@src/utils/dummyData";
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <Text>CategoriesScreen</Text>
+    </View>
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
