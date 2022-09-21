417d25923145b70e43be62df852897fc7e4bd10e
PrettyLegit0101 <55465528+PrettyLegit0101@users.noreply.github.com>
2022-09-20 20:59:05 -0400

    Nested navigators, applied FavoritesContextProvider, added more screens

--- src/App.js
+++ src/App.js
@@ -6,34 +6,83 @@ import MealsOverviewScreen from "@src/screens/MealsOverviewScreen";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import MealDetailScreen from "@src/screens/MealDetailScreen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import FavoritesScreen from "@src/screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
+import FavoritesContextProvider from "@src/store/context/favorites-contexts";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerBackTitle: "Back",
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "#FFF",
+        sceneContainerStyle: { backgroundColor: "#3F2F25" },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="MealsCategories"
-          screenOptions={{
-            headerBackTitle: "Back",
-            headerStyle: { backgroundColor: "#351401" },
-            headerTintColor: "#FFF",
-            contentStyle: { backgroundColor: "#3F2F25" },
-          }}
-        >
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: "All Categories",
+      <FavoritesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="MealsCategories"
+            screenOptions={{
+              headerBackTitle: "Back",
+              headerStyle: { backgroundColor: "#351401" },
+              headerTintColor: "#FFF",
+              contentStyle: { backgroundColor: "#3F2F25" },
             }}
-          />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen name="MealsDetail" component={MealDetailScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+          >
+            <Stack.Screen
+              name="Drawer"
+              component={DrawerNavigator}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MealsOverview"
+              component={MealsOverviewScreen}
+            />
+            <Stack.Screen
+              name="MealsDetail"
+              component={MealDetailScreen}
+              options={{ title: "About the Meal" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </FavoritesContextProvider>
     </>
   );
 }
